f182280e1833dd811e17e1fa853c83ea3e0f98b7
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-06-20 18:50:42 +0200

    comps for navbar

--- src/Pages/dashboard/SharedLayout.jsx
+++ src/Pages/dashboard/SharedLayout.jsx
@@ -3,5 +3,18 @@ import { Navbar, Smallsidebar, BigSidebar } from "../../comps";
 import Wrapper from "../../assets/wrappers/SharedLayout.js";
 
 export default function SharedLayout() {
-  return <Wrapper>SharedLayout</Wrapper>;
+  return (
+    <Wrapper>
+      <main className="dashboard">
+        <Smallsidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
+  );
 }
