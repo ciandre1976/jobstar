0484ecbc068b76f44adb1c7dcfe1cc3a5c73d32f
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-06-21 18:03:02 +0200

    smallsidebar

---
+++ src/utils/links.js
@@ -0,0 +1,28 @@
+import { IoBarChartSharp } from "react-icons/io5";
+import { MdQueryStats } from "react-icons/md";
+import { FaWpforms } from "react-icons/fa";
+import { ImProfile } from "react-icons/im";
+
+const links = [
+  {
+    id: 1,
+    text: "stats",
+    path: "/",
+  },
+  {
+    id: 2,
+    text: "all jobs",
+    path: "all-jobs",
+  },
+  {
+    id: 3,
+    text: "add job",
+    path: "add-job",
+  },
+  {
+    id: 4,
+    text: "profile",
+    path: "profile",
+  },
+];
+export default links;
