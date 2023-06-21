5b2088dbb558d14ee4e44b38d67d095d83e4a17c
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-06-21 16:00:11 +0200

    protected route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import { ProtectedRoute } from "./Pages";
+
 import { Landscape, Error, Register } from "./Pages";
 import {
   AddJob,
@@ -15,7 +17,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Stats />} />
           <Route path="all-jobs" element={<AllJobs />} />
           <Route path="add-job" element={<AddJob />} />
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,5 +1,6 @@
 import Error from "./Error";
 import Landscape from "./Landscape";
 import Register from "./Register";
+import ProtectedRoute from "./ProtectedRoute";
 
-export { Landscape, Error, Register };
+export { Landscape, Error, Register, ProtectedRoute };
