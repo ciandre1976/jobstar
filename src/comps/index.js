dd4e667ef7fea41c5a0ab5f12806bd1f1210378f
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-06-23 18:30:21 +0200

    add job slice

--- src/comps/index.js
+++ src/comps/index.js
@@ -3,5 +3,6 @@ import Logo from "./Logo";
 import Navbar from "./Navbar";
 import Smallsidebar from "./Smallsidebar";
 import Navlinks from "./Navlinks";
+import FormRowSelect from "./FormRowSelect";
 
-export { Logo, Navbar, Smallsidebar, BigSidebar, Navlinks };
+export { Logo, Navbar, Smallsidebar, BigSidebar, Navlinks, FormRowSelect };
